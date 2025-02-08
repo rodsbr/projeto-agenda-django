3cc9c6256d4c61678cc99fec45c8e4b230ac653d
Rodrigo Haneiko <rodsbe@gmail.com>
2025-02-08 20:28:28 -0300

    Criacao de model contact e migration

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
+#id(primary key)
 # Create your models here.
+# first_name(string), last_name(string), phone(string)
+# email(email), created_date(date), description(text)
+
+# Depois
+# category(foreing key), show (boolean), owner (forein key)
+# picture (image)
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField(max_length=255, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
